adfcb456e5de011119172abbbb462b5c3a281918
Phanor Coll <phanorcoll@gmail.com>
2023-01-21 10:01:58 -0400

    📝 Resolve function
    added the resolve function.
    
    ✏️ no task related.

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,3 +1,29 @@
 package routes
 
-func resolve() {}
+import (
+	"net/http"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/labstack/echo/v4"
+	"github.com/phanorcoll/urlshortener/database"
+)
+
+func ResolveURL(c echo.Context) error {
+	url := c.Param("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err != redis.Nil {
+		return c.JSON(http.StatusNotFound, "short not found in the database")
+	} else if err != nil {
+		return c.JSON(http.StatusInternalServerError, "cannot connect to db")
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(301, value)
+
+}
