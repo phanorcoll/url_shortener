6209d63a3bb6f389e4bbffc8d92933a62ed033db
Phanor Coll <phanorcoll@gmail.com>
2023-01-21 11:28:35 -0400

    ♻️ worked on Shortener function
    Completed the shortener function and added the neede packages.
    
    ✏️ No task related.

--- api/go.mod
+++ api/go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.10.0
 )
@@ -15,6 +16,7 @@ require (
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/onsi/gomega v1.23.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,10 +2,15 @@ package routes
 
 import (
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/phanorcoll/urlshortener/database"
 	"github.com/phanorcoll/urlshortener/helpers"
 )
 
@@ -27,7 +32,24 @@ func ShortenUrl(c echo.Context) error {
 	body := new(request)
 
 	if err := c.Bind(body); err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, "cannot parse JSON")
+	}
+
+	//implement rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.RealIP()).Result()
+	if err != redis.Nil {
+		_ = r2.Set(database.Ctx, c.RealIP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.RealIP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			//TODO:
+			// create an object to return, including the rate_limit_rest
+			// limit, _ := r2.TTL(database.Ctx, c.RealIP()).Result()
+			return c.JSON(http.StatusServiceUnavailable, "Rate limit exceeded")
+		}
 	}
 
 	//check if the input is an actual Url
@@ -43,5 +65,47 @@ func ShortenUrl(c echo.Context) error {
 	//enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
 
-	return c.JSON(http.StatusCreated, body)
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.JSON(http.StatusForbidden, "URL custom short is already in use")
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Unable to connect to server")
+	}
+
+	resp := response{
+		URL:            body.URL,
+		CustomShort:    "",
+		Expiry:         body.Expiry,
+		XRateRemaining: 10,
+		RateLimitRest:  30,
+	}
+
+	r2.Decr(database.Ctx, c.RealIP())
+
+	val, _ = r2.Get(database.Ctx, c.RealIP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	// ttl, _ := r2.TTL(database.Ctx, c.RealIP()).Result()
+	// resp.RateLimitRest = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.JSON(http.StatusOK, resp)
 }
